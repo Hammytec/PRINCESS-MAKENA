c4256c05ab5ac8eb4f77e9af769808febd66b901
Chathubbhsll <161918812+Chathubbhsll@users.noreply.github.com>
2024-03-03 15:33:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
+  OWNER_NUMBER: "94761884978",
+  SUDO_NUMBERS : "94761884978,94719574492",
+  OWNER_NAME: "『 chathuboy 』",
   BOT_NAME : "ＺＵＳＹＣＯ",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
