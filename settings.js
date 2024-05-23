cbc266f56b685d9ee080c09dc845a9e541de0e23
botsel1 <sachitharu229@gmail.com>
2024-05-23 22:32:36 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94760498252",
+  OWNER_NUMBER: "94703266372",
   SUDO_NUMBERS : "94760007746,94715346004",
-  OWNER_NAME: "*`ᴍʀ-ᴀꜱʜᴇɴ`*",
+  OWNER_NAME: "*`ᴋᴀᴠᴇʏᴀ ᴍᴏᴅ`*",
   BOT_NAME : "*ᴢᴜꜱʏᴄᴏ ᴍᴅ*",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
